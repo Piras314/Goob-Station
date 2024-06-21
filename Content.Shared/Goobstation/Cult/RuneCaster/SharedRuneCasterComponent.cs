b307ddb92fe5d01430a07a61b0d9bd2022a9ae43
Piras314 <92357316+Piras314@users.noreply.github.com>
2024-06-21 20:23:12 +0000

    Shared stuff

---
+++ Content.Shared/Goobstation/Cult/RuneCaster/SharedRuneCasterComponent.cs
@@ -0,0 +1,78 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Goobstation.Cult.RuneCaster
+{
+    [NetworkedComponent, ComponentProtoName("RuneCaster"), Access(typeof(SharedRuneCasterSystem))]
+    public abstract partial class SharedRuneCasterComponent : Component
+    {
+        public string SelectedState { get; set; } = string.Empty;
+
+        [DataField("color")] public Color Color;
+
+        [Serializable, NetSerializable]
+        public enum RuneCasterUiKey : byte
+        {
+            Key,
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public sealed class RuneCasterSelectMessage : BoundUserInterfaceMessage
+    {
+        public readonly string State;
+        public RuneCasterSelectMessage(string selected)
+        {
+            State = selected;
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public sealed class RuneCasterColorMessage : BoundUserInterfaceMessage
+    {
+        public readonly Color Color;
+        public RuneCasterColorMessage(Color color)
+        {
+            Color = color;
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public enum RuneCasterVisuals
+    {
+        State,
+        Color
+    }
+
+    [Serializable, NetSerializable]
+    public sealed class RuneCasterComponentState : ComponentState
+    {
+        public readonly Color Color;
+        public readonly string State;
+        public readonly int Charges;
+        public readonly int Capacity;
+
+        public RuneCasterComponentState(Color color, string state, int charges, int capacity)
+        {
+            Color = color;
+            State = state;
+            Charges = charges;
+            Capacity = capacity;
+        }
+    }
+
+    [Serializable, NetSerializable]
+    public sealed class RuneCasterBoundUserInterfaceState : BoundUserInterfaceState
+    {
+        public string Selected;
+        public bool SelectableColor;
+        public Color Color;
+
+        public RuneCasterBoundUserInterfaceState(string selected, bool selectableColor, Color color)
+        {
+            Selected = selected;
+            SelectableColor = selectableColor;
+            Color = color;
+        }
+    }
+}
---
+++ Content.Shared/Goobstation/Cult/RuneCaster/SharedRuneCasterSystem.cs
@@ -0,0 +1,4 @@
+namespace Content.Shared.Goobstation.Cult.RuneCaster;
+
+[Virtual]
+public abstract class SharedRuneCasterSystem : EntitySystem { }
