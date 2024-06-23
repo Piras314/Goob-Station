2fc1a21edad9cbc816b62cf1b65a8996f0cc8e75
Piras314 <92357316+Piras314@users.noreply.github.com>
2024-06-23 20:56:06 +0000

    Potential fix for serialization error

--- Content.Shared/Goobstation/Cult/RuneCaster/RuneCasterDoAfterEvent.cs
+++ Content.Shared/Goobstation/Cult/RuneCaster/RuneCasterDoAfterEvent.cs
@@ -1,10 +1,8 @@
 using Content.Shared.DoAfter;
 using Robust.Shared.Map;
-using Robust.Shared.Serialization;
 
 namespace Content.Shared.Goobstation.Cult.RuneCaster;
 
-[Serializable, NetSerializable]
 public sealed partial class RuneCasterDoAfterEvent : SimpleDoAfterEvent
 {
     /// <summary>
